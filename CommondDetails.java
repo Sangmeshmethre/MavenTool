b0a8d3f39fdeaff28eb46aa988da612e160c3600
SangmeshMehter <ssmcmethre3080@gmail.com>
2024-11-18 15:58:43 +0530

    All Commond Details

---
+++ CommondDetails.java
@@ -0,0 +1,14 @@
+ls-------------------for Folder Director
+cd [folder name]---- for Adding new folder
+cd .. -------------- for Root Folder
+git init------------ for To initialized or create Local Repository
+git branch -M [branch name] ---for create new Branch(ex:QuickStart and Webapp);
+git remote add origin [url]--------for To Connet LocalRepository To CenteralRepo
+git status------------------------- for Cheack update status
+git add . ---------------------------for Adding all files in LocalRepository
+git commit -m "commit message"--------for Comming in Local Repository
+git push -u origin [branch name]-------for To Push to Local To Centeral/Remote Repository
+git checkout [Branch Name] ------------for Move one Branch to another branch
+git switch [Branch Name] ---------------for Move one Branch to another brach
+git merge [Branch Name]-----------------for  Merge QuickStart To Webapp
+git pull -------------------------------for  For pull the code
